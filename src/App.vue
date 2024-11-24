d67e715c93a86566076bd3f32aee8d79efb3dd3b
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-24 20:06:22 +0300

    Edit home page design

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,70 @@
 <template>
-  <div
-    class="text-green-600 h-screen flex items-center justify-center font-semibold text-6xl bg-zinc-800"
-  >
-    Hi
+  <div class="bg-zinc-900 h-dvh">
+    <!-- Hero -->
+    <div class="w-full h-[20dvh] bg-black flex items-center justify-center pb-[30px]">
+      <h1 class="font-extrabold text-6xl text-white text-center">
+        <span class="text-[#4EA8DE]">to</span><span class="text-[#8284FA]">do</span>
+      </h1>
+    </div>
+
+    <div class="default-container">
+      <!-- Add Task -->
+      <div class="flex items-center gap-2 relative bottom-[30px]">
+        <input
+          class="w-full py-4 px-6 rounded-lg bg-zinc-800 text-white outline-none focus:outline-none ring-0 focus:ring-0"
+          type="text" placeholder="Add a new task">
+        <button class="p-4 rounded-lg text-white flex items-center gap-2 bg-[#4EA8DE]">
+          Add
+          <PlusCircle class="text-white size-4" />
+        </button>
+      </div>
+
+      <!-- Tasks -->
+      <div class="flex items-center justify-between gap-6 mt-12">
+        <div class="flex items-center gap-2">
+          <span class="text-[#4EA8DE] font-bold">Tasks created</span>
+          <span class="rounded-full text-white bg-zinc-800 px-2 py-1 text-sm font-bold">{{ tasks.length || 0 }}</span>
+        </div>
+
+        <div class="flex items-center gap-2">
+          <span class="text-[#8284FA] font-bold">Completed</span>
+          <span class="rounded-full text-white bg-zinc-800 px-2 py-1 text-sm font-bold">{{ completedTasks }}</span>
+        </div>
+      </div>
+
+      <!-- Task List -->
+      <div class="space-y-3 mt-6">
+        <Todo v-for="task in tasks" :key="task.id"
+          :text="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iusto, quo recusandae ab temporibus dicta id cumque enim voluptatem molestias voluptatibus aperiam culpa ea deleniti repudiandae? Culpa aut debitis molestias.'"
+          :completed="false" />
+
+        <div class="flex flex-col items-center py-10 space-y-6 text-center" v-if="tasks.length === 0">
+          <img :src="ClipboardImage" alt="Empty" class="size-24 object-cover" />
+          <div>
+            <h2 class="text-zinc-500 font-bold text-2xl">You don't have tasks registered yet</h2>
+            <p class="text-zinc-500 text-xl font-light">Create tasks and organize your to-do items</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { PlusCircle } from 'lucide-vue-next';
+import Todo from '@/components/Todo.vue';
+import { onMounted, ref } from 'vue';
+import type { TaskProps } from '@/types';
+import ClipboardImage from '@/assets/clipboard.png'
+
+const tasks = ref<TaskProps[]>([]);
+const completedTasks = ref<number>(0);
+
+onMounted(() => {
+  let data = localStorage.getItem('tasks')
+  if (data) {
+    tasks.value = JSON.parse(data)
+    tasks.value.filter(task => task.completed) || 0;
+  }
+})
+</script>
